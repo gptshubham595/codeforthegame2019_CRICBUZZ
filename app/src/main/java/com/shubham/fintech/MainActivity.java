d2e67774833b32c907da9db845d448489fb1691b
gptshubham595 <gptshubham595@gmail.com>
2019-06-13 23:34:27 +0530

    Game

---
+++ app/src/main/java/com/shubham/fintech/MainActivity.java
@@ -0,0 +1,18 @@
+package com.shubham.fintech;
+
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public abstract class MainActivity extends AppCompatActivity {
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+}}
